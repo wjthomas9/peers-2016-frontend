9598b896df0e28ec05acd7795db35a9ab33ad93d
Jon Thomas <jon@analyticl.com>
2016-04-11 13:22:17 -0500

    Add masonry

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,9 @@
-//= include ...jquery
+//= include ../bower_components/masonry/dist/masonry.pkgd.js
+
+var elem = document.querySelector('.masonry-grid');
+
+var msnry = new Masonry( elem, {
+    itemSelector: '.masonry-grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+});
